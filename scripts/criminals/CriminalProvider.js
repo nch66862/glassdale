a1b06f07a6ad85432393375ca55cf6680261849a
Nick Carver <nicholascarver2@outlook.com>
2021-01-25 15:07:10 -0600

    saving my place before going to chapter 4

--- scripts/criminals/CriminalProvider.js
+++ scripts/criminals/CriminalProvider.js
@@ -12,7 +12,6 @@ export const getCriminals = () => {
   return fetch("https://criminals.glassdale.us/criminals")
     .then(response => response.json())
     .then(parsedResponse => {
-      console.table(parsedResponse)
       criminals = parsedResponse
     })
 
