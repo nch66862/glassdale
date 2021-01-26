0f42bc27f88ff7eaed8640cd528001acfc182527
Nick Carver <nicholascarver2@outlook.com>
2021-01-26 17:18:35 -0600

    correctly renders the criminals when they are filtered by conviction

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -22,22 +22,32 @@ eventHub.addEventListener("crimeChosen", event => {
 
     if (event.detail.crimeThatWasChosen !== "0"){
 
+      // Get a copy of the array of convictions from the data provider
+      const convictionsArray = useConvictions()
+
+      // Use the find method to get the first object in the convictions array that has the same id as the id of the chosen crime
+      const convictionThatWasChosen = convictionsArray.find(convictionObj => {
+        return convictionObj.id === parseInt(event.detail.crimeThatWasChosen)
+      }
+      )
+
+
         const matchingCriminals = criminalArray.filter(matchingCriminal => {
           // debugger
           //crime that was chosen is a number that represents a crime
           //need to correlate number to meaning
           //meaning will correspond with matchingcriminal.conviction
-          let objectInDesiredIndex = {}
-          const indexNumber = event.detail.crimeThatWasChosen - 1
-          const convictionList = useConvictions()
-          const desiredIndex = convictionList.slice(indexNumber, 1)
-          const arrayIteratorInDesiredIndex = desiredIndex.values()
-          for (const object of arrayIteratorInDesiredIndex) {
-            objectInDesiredIndex = object;
-          }
-          const correlatingCrime = objectInDesiredIndex.name
+          // let objectInDesiredIndex = {}
+          // const indexNumber = event.detail.crimeThatWasChosen - 1
+          // const convictionList = useConvictions()
+          // const desiredIndex = convictionList.slice(indexNumber, 1)
+          // const arrayIteratorInDesiredIndex = desiredIndex.values()
+          // for (const object of arrayIteratorInDesiredIndex) {
+          //   objectInDesiredIndex = object;
+          // }
+          // const correlatingCrime = objectInDesiredIndex.name
 
-          return matchingCriminal.conviction == correlatingCrime
+          return matchingCriminal.conviction === convictionThatWasChosen.name
         })
 
        render(matchingCriminals)
