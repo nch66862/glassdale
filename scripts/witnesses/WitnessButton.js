aa9597f99bea034a9e002c77e70bfbfc9d1c4a39
Nick Carver <nicholascarver2@outlook.com>
2021-02-01 10:23:08 -0600

    moved a button to a new container. Added a new event listener inside the criminalList component for my toggle buttons

--- scripts/witnesses/WitnessButton.js
+++ scripts/witnesses/WitnessButton.js
@@ -1,15 +1,9 @@
 const witnessButton = '<button class="witnessCriminalToggle">Show Witnesses</button>'
 
 export const ShowWitnessButton = () => {
-    const contentTarget = document.querySelector(".peopleContainer")
-    contentTarget.insertAdjacentHTML('afterbegin', witnessButton)
+    const contentTarget = document.querySelector("nav")
+    contentTarget.innerHTML = witnessButton
 }
-
-// export const criminalStatusCommunicator = (criminalsHaveRendered) => {
-//     if (criminalsHaveRendered === true) {
-
-//     }
-// }
 
 const eventHub = document.querySelector(".container") //specify the outer container as the event hub
 
@@ -19,11 +13,3 @@ eventHub.addEventListener("click", clickEvent => {
         eventHub.dispatchEvent(customEvent)
     }
 })
-
-// eventHub.addEventListener("witnessToggleClicked", event => { //listen for the custom event created above
-//         const contentTarget = document.querySelector(".witnessCriminalToggle")
-//         if (document.querySelector(".peopleContainer").contains(""))
-//         render(matchingCriminals) //put those filtered criminals on the web page
-    
-// }
-// )  
