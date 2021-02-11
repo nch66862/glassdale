9731861d7090fe15810053c072c1c200b855628d
Nick Carver <nicholascarver2@outlook.com>
2021-02-11 15:57:56 -0600

    fixed the date issue. Shows up on notes list as correct day

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,8 +1,8 @@
 export const NoteHTMLConverter = (noteObject, criminalArray) => {
-    const relatedCriminal = criminalArray.find(criminal => criminal.id === parseInt(noteObject.criminalID))
+    const relatedCriminal = criminalArray.find(criminal => criminal.id === noteObject.criminalID)
     return `
         <section class="note" id="noteID=${noteObject.id}">
-            <div class="note__date">Timestamp: ${ new Date(noteObject.date).toLocaleDateString('en-US')  }</div>
+            <div class="note__date">Timestamp: ${ new Date(noteObject.date).toLocaleDateString('en-US', { timeZone: "UTC" })  }</div>
             <div class="note__subject">Subject: ${ noteObject.subject }</div>
             <div class="note__criminal">Suspect: ${ relatedCriminal.name }</div>
             <div class="note__note">${ noteObject.note }</div>
