eb64f1da9ec0133c363eff9711f8a57e08e3d2eb
Nick Carver <nicholascarver2@outlook.com>
2021-02-12 12:03:11 -0600

    wrote some code.it is lumchtime'

--- scripts/facilities/FacilityList.js
+++ scripts/facilities/FacilityList.js
@@ -1,22 +1,24 @@
-import { getWitnesses, useWitnesses } from "./WitnessDataProvider.js"
-import { Witness } from "./Witness.js"
+import { Facility } from "./Facility.js"
+import { getFacilities, useFacilities } from "./FacilityProvider.js"
 
 const eventHub = document.querySelector(".container") //specify the outer container as the event hub
 const targetContainer = document.querySelector(".peopleContainer")
 
 const FacilityList = () => {
-    getWitnesses()
+    getFacilities()
         .then(() => {
-            const witnessArray = useWitnesses()
-            let witnessHTMLRepresentations = ""
-            for (const witness of witnessArray) {
-                witnessHTMLRepresentations += Witness(witness)
-            }
+            const facilityArray = useFacilities()
+            const facilityHTMLRepresentations = facilityArray.map(facility => {
+
+                
+
+                return Facility(facility, criminalsInside)
+            }).join("")
             targetContainer.innerHTML = `
-        <article class="witnessContainer">
-            <h3>Cold Case Witnesses</h3>
-            <section class="witnessList">
-                ${witnessHTMLRepresentations}
+        <article class="facilityContainer">
+            <h3>Facility Database</h3>
+            <section class="facilityList">
+                ${facilityHTMLRepresentations}
             </section>
         </article>
         `
@@ -24,7 +26,7 @@ const FacilityList = () => {
 }
 
 eventHub.addEventListener("showFacilitiesClicked", event => { //listen for the custom event from ConvictionSelect.js
-    // FacilityList()
+    FacilityList()
     const customEvent = new CustomEvent("peopleContainerChanged", { //create a custom event.
         detail: {
             informationRendered: "Facilities" //assigns the value specified in each option element to this variable officerThatWasChosen
