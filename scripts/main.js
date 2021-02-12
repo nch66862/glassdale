6175ab941bc8c8d2ef2dd67d3a1715bdc8857f53
Nick Carver <nicholascarver2@outlook.com>
2021-02-12 14:07:07 -0600

    the officers and their arrested criminals render to the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ import "./notes/NoteList.js";
 import "./criminals/CriminalAlibiList.js"
 import "./witnesses/WitnessList.js"
 import "./facilities/FacilityList.js"
+import "./officers/OfficerList.js"
 import { ShowHeader } from "./header/Header.js";
 import { ShowWitnessButton } from "./witnesses/WitnessButton.js";
 import { ShowCriminalButton } from "./criminals/CriminalButton.js";
--- scripts/officers/OfficerList.js
+++ scripts/officers/OfficerList.js
@@ -2,46 +2,23 @@ import { getOfficers, useOfficers } from "./OfficerProvider.js"
 import { Officer } from "./Officer.js"
 import { useCriminals } from "../criminals/CriminalProvider.js"
 
-export const OfficerList = () => {
-  const officersContainer = document.querySelector(".officersContainer")
-  getOfficers()
-    .then(() => {
-      const officerArray = useOfficers()
-      let officersHTMLRepresentations = ""
-      for (const officer of officerArray) {
-        officersHTMLRepresentations += Officer(officer)
-      }
-      officersContainer.innerHTML = `
-        <h3>Glassdale Officers</h3>
-        <section class="officersList">
-        ${officersHTMLRepresentations}
-        </section>`
-    })
-}
-
-
 const eventHub = document.querySelector(".container") //specify the outer container as the event hub
 const targetContainer = document.querySelector(".peopleContainer")
 
 const OfficerList = () => {
-    getFacilities()
+    getOfficers()
         .then(() => {
-            const facilityArray = useFacilities()
-            const criminalFacilitesRelationships = useCriminalFacilities()
+            const officerArray = useOfficers()
             const criminalArray = useCriminals()
-            const facilityHTMLRepresentations = facilityArray.map(facility => {
-                const criminalsInFacility = criminalFacilitesRelationships.filter(relationshipObj => relationshipObj.facilityId === facility.id)
-                const criminalsInside = criminalsInFacility.map(crimFacRel => {
-                    const matchingCriminal = criminalArray.find(criminal => criminal.id === crimFacRel.criminalId)
-                    return matchingCriminal.name
-                })
-                return Facility(facility, criminalsInside)
+            const officerHTMLRepresentation = officerArray.map(officerObj => {
+                const arrayOfArrestedCriminals = criminalArray.filter(criminalObj => criminalObj.arrestingOfficer === officerObj.name)
+                return Officer(officerObj, arrayOfArrestedCriminals)
             }).join("")
             targetContainer.innerHTML = `
-        <section class="facilityContainer">
-            <h3>Facility Database</h3>
-            <section class="facilityList">
-                ${facilityHTMLRepresentations}
+        <section class="officerContainer">
+            <h3>Officer Database</h3>
+            <section class="officerList">
+                ${officerHTMLRepresentation}
             </section>
         </section>
         `
